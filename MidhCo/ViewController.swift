dfd8a9a23b4ae22fed34daa75c317949da46adce
Mahgol Fathi <m_fathi@arsh.co>
2018-10-27 12:23:50 +0330

    First commit

---
+++ MidhCo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MidhCo
+//
+//  Created by Mahgol Fathi on 10/27/18.
+//  Copyright © 2018 Mahgol Fathi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
